e4a3e9908928433b7fc7cc16854a8e5d000c2854
joseph <joseph_vv@hotmail.com>
2022-11-13 02:41:14 -0300

    success animation

--- src/components/ShowcaseApp.js
+++ src/components/ShowcaseApp.js
@@ -1,26 +1,45 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 function ShowcaseApp() {
 
-    const successRate = 0.7;
-    const [currentLevel, setCurrentLevel] = useState(0)
-    
+    const successRate = 0.75;
+    const [currentLevel, setCurrentLevel] = useState(0);
+    const [resultText, setResultText] = useState("FAIL");
+    const [resultStyle, setResultStyle] = useState("result-popout");
 
     const levelup = ()=>{
         let randomVal = Math.random();
         if(randomVal <= successRate){
             setCurrentLevel((previousLevel)=> previousLevel+1);
+            setResultStyle((e)=>(e+" result-enable good-result"));
+            setResultText("SUCCESS");
         }
-        else setCurrentLevel(0);
+        else{
+          setResultText("FAIL");
+          setCurrentLevel(0);
+          setResultStyle((e)=>(e+" result-enable bad-result"));
+        }
 
+        setTimeout(()=>{
+          setResultStyle("result-popout");
+        }
+        , 500);
     }
+
 
   return (
     <div className='sc-app-container'>
-      <img className='sc-item' src="https://cdn-icons-png.flaticon.com/512/1213/1213817.png?w=740&t=st=1668230181~exp=1668230781~hmac=db5ae83583d58e4b698a34d38451013a46e19273d760442cccdf118d025bda4e" alt="item" />
-      <div className='sc-level-info'>Level: <span>{currentLevel}</span></div>
-      <div className='sc-sucess-info'>Success rate: <span>70%</span></div>
+      <div className='sc-item'>
+        <img className='' src="https://cdn-icons-png.flaticon.com/512/1213/1213817.png?w=740&t=st=1668230181~exp=1668230781~hmac=db5ae83583d58e4b698a34d38451013a46e19273d760442cccdf118d025bda4e" alt="item" />
+        <div>item name</div>
+        <div className='sc-level-info'>Level: <span>{currentLevel}
+        </span></div>
+      </div>
+      
+        <div className='sc-sucess-info'>Success rate: <span>{`${successRate*100}%`}</span></div>
       <div onClick={levelup} className='lvlupButton'>LVL UP</div>
+
+      <div className={resultStyle}>{resultText}</div>      
     </div>
   )
 }
